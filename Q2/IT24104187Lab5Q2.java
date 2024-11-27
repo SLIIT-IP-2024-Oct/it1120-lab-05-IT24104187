8706f1774125d573579ef7f59e67c51732faf1e7
IT24104187 <it24104187@my.sliit.lk>
2024-11-27 21:41:56 +0530

    Add files via upload

---
+++ Q2/IT24104187Lab5Q2.java
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+import java.util.Scanner;
+
+public class IT24104187Lab05Q2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of new members: ");
+        int newMembers = scanner.nextInt();
+
+        if (newMembers < 0) {
+            System.out.println("Invalid number. The number of new members should be greater than or equal to 0.");
+        } else {
+            String prize;
+            switch (newMembers) {
+                case 0:
+                    prize = "No Prize";
+                    break;
+                case 1:
+                    prize = "Pen";
+                    break;
+                case 2:
+                    prize = "Umbrella";
+                    break;
+                case 3:
+                    prize = "Bag";
+                    break;
+                case 4:
+                    prize = "Travelling Chair";
+                    break;
+                default:
+                    prize = "Headphone";
+                    break;
+            }
+            System.out.println("Prize: " + prize);
+        }
+
+        scanner.close();
+    }
+}
+
+
+
